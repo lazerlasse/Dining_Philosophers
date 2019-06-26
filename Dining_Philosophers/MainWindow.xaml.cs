766ea72ac15ad02e36925a002fcb5d652981f501
lazerlasse <31047935+lazerlasse@users.noreply.github.com>
2019-06-26 08:53:12 +0200

    Created a small welcome in mainWindow and initialized PhilosophersView from Main.

--- Dining_Philosophers/MainWindow.xaml.cs
+++ Dining_Philosophers/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Dining_Philosophers.Views;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,13 @@ namespace Dining_Philosophers
 		public MainWindow()
 		{
 			InitializeComponent();
+			PhilosophersView View = new PhilosophersView();
+			View.Show();
+		}
+
+		private void Button_Click(object sender, RoutedEventArgs e)
+		{
+			this.Close();
 		}
 	}
 }
